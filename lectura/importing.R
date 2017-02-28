b58435858ded8453e748f18ee2904375c0370798
Juan Camilo Herrera <juan.c.herrerav@hotmail.com>
2017-02-28 10:58:38 -0500

    mas export e import

--- lectura/importing.R
+++ lectura/importing.R
@@ -1,3 +1,6 @@
+## ~ Se usa para moverse en todos los directorios hasta la carpeta Documents
+## <- Se usa para asignar lo que lee a la variable prueba
+
 ##Set the working directory
 setwd("/Users/JuanHerrera/Documents/ESTI")
 ##Get current working directory
@@ -10,26 +13,41 @@ install.packages("rJava")
 install.packages("xlsxjars")
 
 ##Import excel files
+#Se importa la libreria readxl para leer archivos de excel
 library(readxl)
-prueba <- read_excel("~/Documents/ESTI/prueba.xlsx")
+prueba <- read_excel("~/Documents/ESTI/archivos/prueba.xlsx")
+#View se usa para mostrar prueba en pantalla
 View(prueba)
 
 ##Import csv files
+#Se importa la libreria readr para leer archivos csv
 library(readr)
-prueba2 <- read_csv("~/Documents/ESTI/prueba2.csv")
+prueba2 <- read_csv("~/Documents/ESTI/archivos/prueba.csv")
 View(prueba2)
 
 ##Import txt files
-hola <- read.table("~/Documents/ESTI/hola.txt")
+hola <- read.table("~/Documents/ESTI/archivos/Hola.txt")
 View(hola)
 
+##Importy sav files
+#Se importa la libreria foreign para leer archivos sav
+library(foreign)
+prueba3 <- read.spss("~/Documents/ESTI/archivos/base.sav")
+View(prueba3)
+
 ##Export xlsx files
+#Se importa la libreria xlsx para exportar archivos de excel
 library(xlsx)
-wb <- createWorkbook(type = "xlsx")
-saveWorkbook(wb,"~/Documents/ESTI/pruebaExport.xlsx")
+write.xlsx(prueba2, "~/Documents/ESTI/archivos/pruebaExport.xlsx")
 
 ##Export txt files
-write.table(hola,"~/Documents/ESTI/prubeaExport1.txt")
+## Se usa write.table para escribir archivos planos
+write.table(hola,"~/Documents/ESTI/archivos/prubeaExport1.txt")
 
 ##Export csv files
-write.csv(hola,"~/Documents/ESTI/pruebaExport.csv")
+## Se usa write.csv para escribir archivos de formato csv
+write.csv(hola,"~/Documents/ESTI/archivos/pruebaExport.csv")
+
+##Export sav files
+##Se usa la libreria foreign para exportar archivos de formato sav
+library(foreign)
