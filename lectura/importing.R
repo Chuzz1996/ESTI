4a0569a87bb0777c5a8d75f71279a1c52f547407
Juan Camilo Herrera <juan.c.herrerav@hotmail.com>
2017-04-27 18:38:16 -0500

    coneccion a db

--- lectura/importing.R
+++ lectura/importing.R
@@ -11,13 +11,15 @@ install.packages("csv")
 install.packages("sas7bdat")
 install.packages("rJava")
 install.packages("xlsxjars")
+install.packages("RJDBC")
+install.packages("DBI")
 
 ##Import excel files
 #Se importa la libreria readxl para leer archivos de excel
 library(readxl)
-prueba <- read_excel("~/ESTI-Master/archivos/prueba.xlsx")
+divipola <- read_excel("~/Documents/ESTI-Master/archivos/Listado_2012.xlsx")
 #View se usa para mostrar prueba en pantalla
-View(prueba)
+View(divipola)
 
 ##Import csv files
 #Se importa la libreria readr para leer archivos csv
@@ -51,3 +53,21 @@ write.csv(hola,"~/ESTI-Master/archivos/pruebaExport.csv")
 ##Export sav files
 ##Se usa la libreria foreign para exportar archivos de formato sav
 library(foreign)
+
+#Conecci??n a DB
+Sys.setenv(JAVA_HOME='/path/to/java_home')
+options(java.parameters="-Xmx2g")
+library(rJava)
+
+.jinit()
+
+library(DBI)
+library(RJDBC)
+drv <- JDBC("oracle.jdbc.OracleDriver","/Users/JuanHerrera/Documents/ESTI-Master/ojdbc6.jar")
+con <- dbConnect(drv, "jdbc:oracle:thin:@//granate.is.escuelaing.edu.co:1521", "bd2103021","bd2103021")
+
+
+dbWriteTable(con, "TEST_TABLE", test_table)
+dbGetQuery(con, "select count(*) from MUNICIPIO")
+d <- dbReadTable(con, "TEST_TABLE")
+dbDisconnect(con)
